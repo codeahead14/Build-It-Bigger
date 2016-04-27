e9570d7616b4a3803b941d91614b8dd0e67d4608
codeahead <gaurav.jain14@hotmail.com>
2016-04-27 21:46:33 +0530

    Setting Up google Cloud Enpoints

---
+++ javajokelibrary/src/main/java/com/example/JokeLib.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import java.io.IOException;
+
+import javax.naming.Context;
+
+import javafx.util.Pair;
+
+public class JokeLib {
+
+    public String getJoke(){
+        return "You are a masterPiece!!";
+    }
+}
+
